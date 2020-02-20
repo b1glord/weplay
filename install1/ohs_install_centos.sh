099f14b374fd76a28a36fd7db5684f290aa6a47e
BigLorD <FuryTr@yandex.com>
2020-02-20 17:10:58 +0300

    Update ohs_install_centos.sh

--- install1/ohs_install_centos.sh
+++ install1/ohs_install_centos.sh
@@ -1,8 +1,7 @@
 #yum groupinstall "Development Tools"
 yum install -y gcc-c++ mariadb-libs mariadb-devel
 yum install -y screen tar wget unzip bzip2 bzip2-devel gmp-devel
-ln -s /usr/lib64/mysql/libmysqlclient.so /usr/lib64/libmysqlclient.so
-
+ln -s /usr/lib64/mysql/libmysqlclient.so /usr/lib/libmysqlclient.so
 
 # Installing Git New Version
 yum remove -y git
@@ -15,7 +14,7 @@ cd /home
 wget http://downloads.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2
 tar xjf boost_1_54_0.tar.bz2
 cd /home/boost_1_54_0
-./bootstrap.sh --with-libraries=filesystem,program_options,system,regex,thread,date_time --libdir=/usr/lib64/
+./bootstrap.sh --with-libraries=filesystem,program_options,system,regex,thread,date_time --libdir=/usr/lib/
 ./bjam
 ./bjam install
 
@@ -25,14 +24,14 @@ git clone https://github.com/b1glord/OHSystem.git
 cd /home/OHSystem/ghost/src/bncsutil/src/bncsutil
 make
 make install
-cp /home/OHSystem/ghost/src/bncsutil/src/bncsutil/libbncsutil.so /usr/lib64/libbncsutil.so
-ln -s /home/OHSystem/ghost/src/bncsutil/src/bncsutil/libbncsutil.so /usr/lib/libbncsutil.so
+cp /home/OHSystem/ghost/src/bncsutil/src/bncsutil/libbncsutil.so /usr/lib/libbncsutil.so
+ln -s /usr/lib/libbncsutil.so /usr/lib64/libbncsutil.so
 
 cd /home/OHSystem/ghost/src/StormLib/stormlib
 make
 make install
-cp /home/OHSystem/ghost/src/StormLib/stormlib/libStorm.so /usr/lib64/libStorm.so
-ln -s /home/OHSystem/ghost/src/StormLib/stormlib/libStorm.so /usr/lib/libStorm.so
+cp /home/OHSystem/ghost/src/StormLib/stormlib/libStorm.so /usr/lib/libStorm.so
+ln -s /usr/lib/libStorm.so /usr/lib64/libStorm.so
 cd /home/OHSystem/ghost/src
 make
 
